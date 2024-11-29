c4104cc31877f34e6ba63311b9d9b66b490aaa6c
Leonardo <leo.arauj@outlook.com>
2024-11-28 22:32:08 -0300

    Entrega da tarefa 10001 - Implementar o cadastro de produtos

--- main.py
+++ main.py
@@ -1,36 +1,22 @@
 from services.estoque_service import cadastrar_estoque
 
-nome = ""
-categoria = ""
-quantidade = 0
-
 label_nome_produto = "Nome do Produto"
 label_categoria_produto = "Categoria"
 label_quantidade_produto = "Quantidade"
 label_preco_produto = "Preço"
 label_localizacao = "Localização"
 
-def tratar_campo(campo, descricao):
+def verificar_campo_vazio(campo, descricao): 
     if isinstance(campo, (int, float, complex)):
         if not campo:
             print(f"O campo '{descricao}' está vazio, por favor preencha!")
-            try: 
-                #campo = int(input(f"- {descricao}: "))
-                return int(input(f"- {descricao}: "))
-            except ValueError:
-                print("Operação cancelada!")
-                return True
+            return True
 
     elif not campo or len(campo) == 0:
         print(f"O campo '{descricao}' está vazio, por favor preencha!")
-        campo = input(f"- {descricao}2: ")
-        print(f"Valor Campo: {campo}")
-
-        if len(campo) == 0:
-            print("Operação cancelada!")
-            return
-        else:
-            return campo
+        return True
+    else:
+        return False
 
 def formata_label(label):
     return f"- {label}: "
@@ -44,53 +30,49 @@ def main():
         print(" * 0. Sair")
         opcao = input("Escolha uma opção: ")
 
-        if opcao == "1":
-            nome = input(formata_label(label_nome_produto))
-            if not nome:
-                nome = tratar_campo(nome, label_nome_produto)
+        match opcao:
+            case '1':
+                nome = input(formata_label(label_nome_produto))
+                if verificar_campo_vazio(nome, label_nome_produto):
+                    nome = input(formata_label(label_nome_produto))
+                    if not nome:
+                        print("Operação cancelada!")
+                        continue
                 
+                categoria = input(formata_label(label_categoria_produto))
+                if verificar_campo_vazio(categoria, label_categoria_produto):
+                    categoria = input(formata_label(label_categoria_produto))
+                    if not categoria:
+                        print("Operação cancelada!")
+                        continue
 
-            print(f"nome:: {nome}")
+                quantidade = input(formata_label(label_quantidade_produto))
+                if verificar_campo_vazio(quantidade, label_quantidade_produto):
+                    quantidade = input(formata_label(label_quantidade_produto))
+                    if not quantidade or quantidade == 0:
+                        print("Operação cancelada!")
+                        continue
 
+                preco = input(formata_label(label_preco_produto))
+                if verificar_campo_vazio(preco, label_preco_produto):
+                    preco = input(formata_label(label_preco_produto))
+                    if not preco or preco == 0:
+                        print("Operação cancelada!")
+                        continue
 
-            categoria = input(formata_label(label_categoria_produto))
-            if tratar_campo(categoria, label_categoria_produto):
+                localizacao = input(formata_label(label_localizacao))
+                if verificar_campo_vazio(localizacao, label_localizacao):
+                    localizacao = input(formata_label(label_localizacao))
+                    if not localizacao:
+                        print("Operação cancelada!")
+                        continue
+
+                cadastrar_estoque(estoque, nome, categoria, quantidade, preco, localizacao)
+            case '0':
+                print("Saindo do sistema!")
                 break
-            print(f"categoria:: {categoria}")
-
-
-
-            try:
-                quantidade = int(input(formata_label(label_quantidade_produto)))
-            except ValueError:
-                 quantidade = 0
-                 print("Entrada inválida! Por favor, digite um número inteiro.")
-
-            if tratar_campo(quantidade, label_quantidade_produto): 
-                break
-
-
-            try:
-                preco = float(input(formata_label(label_preco_produto)))
-            except ValueError:
-                preco = 0
-                print("Entrada inválida! Por favor, digite um número inteiro ou decial.")
-
-            if tratar_campo(preco, label_preco_produto):
-                break
-
-
-            localizacao = input(formata_label(label_localizacao))
-            if tratar_campo(localizacao, label_localizacao): 
-                break
-
-            cadastrar_estoque(estoque, nome, categoria, quantidade, preco, localizacao)
-
-        elif opcao == "0":
-            print("Saindo do sistema!")
-            break
-        else:
-            print("Essa opção não existe, por favor tente novamente!")
+            case _:
+                print("Essa opção não existe, por favor tente novamente!")
 
 if __name__ == "__main__":
     main()
